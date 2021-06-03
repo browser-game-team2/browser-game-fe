fe69fe684a3895ffe00305c8ccdc6a214a455b68
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-06-04 00:03:15 +0200

    fist-random-enemy-army-generation

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ googleAuthentication.addEventListener("click", changeBoxContent);
 confirmButton.addEventListener("click", changeBoxContent2);
 startGame.addEventListener("click", boardGenerator);
 
+
+const armySelection = {
+    "SpaceShip": 1,
+    "spaceCruiser": 2,
+    "spaceDestroyer": 5,
+}
+
+let gold = 10;
+let goldCpu = 10;
 //after the ouath, display inputs to choose nickname and planet name
 
 function changeBoxContent() {
@@ -22,60 +31,56 @@ function changeBoxContent() {
     inputs.style.display = "flex";
 }
 //display nickname and planet name choosen by user and the quantity of gold to craft the army
-let gold = 10;
+
 
 function changeBoxContent2(){
     inputs.style.display = "none";
     createArmy.style.display = "flex";
     userName.innerHTML = "Username:" + " " + document.getElementById("input__username").value;
     planetName.innerHTML = "Planet name:" + " " + document.getElementById("input__planet__name").value;
-    resources.innerHTML = "You Have " + gold + " Gold to craft your army";
 }
 
 //each unity has a cost to subtract from the total of gold
+class Unities {
+    constructor (name, power, cost) {
+        this.name = name;
+        this.power = power;
+        this.cost = cost;
+    }
+    buyShip() {
+        if (gold >= this.cost) {
+            gold = gold - this.cost;
+            resources.innerHTML = "You Have " + gold + " Gold to craft your army";
+            army.push(this.name);
+            } else (resources.innerHTML = "You can't spend more than 10 gold to craft your army")
+        }
+    }
 
-function Unities(name, power, cost) {
-    this.name = name;
-    this.power = power;
-    this.cost = cost;
-  }
-const spaceShip = new Unities ("Space Ship", "??", "1");
-const spaceCruiser = new Unities ("Space Cruiser", "??", "2");
-const spaceDestroyer = new Unities ("Space Destroyer", "??", "5");
 let army = [];
 
-document.getElementById("space__ship").addEventListener("click", buySpaceShip);
-document.getElementById("space__cruiser").addEventListener("click", buySpaceCruiser);
-document.getElementById("space__destroyer").addEventListener("click", buySpaceDestroyer);
+document.querySelectorAll(".army__unities").forEach(e => e.addEventListener("click", buyArmy));
 
-function buySpaceShip(){
-    if (gold >= spaceShip.cost) {
-    gold = gold - spaceShip.cost;
-    resources.innerHTML = "You Have " + gold + " Gold to craft your army";
-    army.push("1S");
-    console.log(army);
-    } else (alert("You can't spend more than 10 gold to craft your army"))
-}
-
-function buySpaceCruiser(){
-    if (gold >= spaceCruiser.cost) {
-    gold = gold - spaceCruiser.cost;
-    resources.innerHTML = "You Have " + gold + " Gold to craft your army";
-    army.push("1C");
-    console.log(army);
-    } else (alert("You can't spend more than 10 gold to craft your army"))
-}
-function buySpaceDestroyer(){
-    if (gold >= spaceDestroyer.cost) {
-    gold = gold - spaceDestroyer.cost;
-    resources.innerHTML = "You Have " + gold + " Gold to craft your army";
-    army.push("1D");
-    console.log(army);
-    } else (alert("You can't spend more than 10 gold to craft your army"))
+function buyArmy() {
+    // I generate the object taking the first letter with slice and the number (the cost)
+    console.log(parseInt(this.innerText.match(/\d+/)[0]));
+    const spaceShip = new Unities (this.innerText.slice(0,1), "??", parseInt(this.innerText.match(/\d+/)[0]));
+    spaceShip.buyShip();
 }
 
 
 function boardGenerator() {
-    
+    let cpuArmy = [];
+    let goldCpu = 10;
+    while(goldCpu > 0) {
+        const army = Object.keys(armySelection);
+        const randomArmy = army[Math.floor(Math.random() * army.length)];
+        const armyCost = armySelection[randomArmy];
+        const spaceShip = new Unities (randomArmy, "??", armyCost, goldCpu);
+        spaceShip.buyShip();
+        cpuArmy.push(randomArmy);
+        goldCpu -= armyCost;
+        console.log(goldCpu );
+    }
+    console.log(cpuArmy);
 }
 

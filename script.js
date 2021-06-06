8be2b28fee4703e9878b8ac2593ba3027a59dabf
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-06-06 20:15:01 +0200

    oop-code-game-complete-refactor

--- script.js
+++ script.js
@@ -14,100 +14,113 @@ facebookAuthentication.addEventListener("click", changeBoxContent);
 googleAuthentication.addEventListener("click", changeBoxContent);
 confirmButton.addEventListener("click", changeBoxContent2);
 
-let cpuArmy = [];
-let humanArmy = [];
 
-const armySelection = {
-    "SpaceShip": 1,
-    "spaceCruiser": 2,
-    "spaceDestroyer": 5,
+
+
+
+class Unities {
+    constructor (army, cost) {
+        this.army = army;
+        this.cost = cost;
+    }
 }
 
-//after the ouath, display inputs to choose nickname and planet name
+class composeArmy {
+    constructor(type) {
+        this.type = type;
+        this.army = [];
+    }
+    create() {
+        let gold = 30;
+        const event = this;
+        const armySelection = {
+            "SpaceShip": 1,
+            "spaceCruiser": 2,
+            "spaceDestroyer": 5,
+        }
+        if(this.type === "Human") {
+            document.querySelectorAll(".army-type").forEach(e => e.addEventListener("click", function() {
+                const armyCost = armySelection[this.innerText];
+                if(event.army.length < 10 && gold > armyCost) {
+                    event.army.push(new Unities(this.innerText, armyCost));
+                    gold -= armyCost;
+                    resources.innerHTML = "You Have " + gold + " Gold to craft your army";
+                } else {
+                    return resources.innerHTML = "You can't spend more than " + 30 + "gold to craft your army"
+                }
+            }));  
+            return event.army;
+        } else {
+            while(gold > 0 && event.army.length < 10) {
+                const army = Object.keys(armySelection);
+                //take a random type of army and find the cost in armyCost;
+                const randomArmy = army[Math.floor(Math.random() * army.length)];
+                const armyCost = armySelection[randomArmy];
+                
+                if(gold >= armyCost) {
+                    event.army.push(new Unities(randomArmy, armyCost));
+                    gold -= armyCost;
+                    
+                } else {
+                    // if the armyCost is bigger than goldCpu then call the function again to generate another random army
+                    return create();
+                }
+            }
+            return event.army;
+        }
+         
+    }
+}
 
+class Player {
+    constructor(type, name, email, planet) {
+        this.type = type;
+        this.name = name;
+        this.email = email;
+        this.planet = planet;
+        this.playerArmy = [];
+    }
+}
+
+class gameBoard {
+    constructor() {
+        this.players = [];
+    }
+    createGame() {
+        console.log(this.players);
+    } 
+}
+
+const humanArmy = new composeArmy("Human");
+const cpuArmy = new composeArmy("CPU");
+cpuArmy.create()
+humanArmy.create();
+const game = new gameBoard();
+
+
+
+//after the ouath, display inputs to choose nickname and planet name
 function changeBoxContent() {
     authentication.style.display = "none";
     inputs.style.display = "flex";
 }
+
+
 //display nickname and planet name choosen by user and the quantity of gold to craft the army
-
-
 function changeBoxContent2(){
     inputs.style.display = "none";
     createArmy.style.display = "flex";
     userName.innerHTML = "Username:" + " " + document.getElementById("input__username").value;
     planetName.innerHTML = "Planet name:" + " " + document.getElementById("input__planet__name").value;
+    game.players.push(new Player("Human", document.getElementById("input__username").value, "test@gmail.com", document.getElementById("input__planet__name").value));
+    game.players.push(new Player("CPU", "Computer1", "", "Venus"));
+   
 }
 
-//each unity has a cost to subtract from the total of gold
-class Unities {
-    constructor (type, name, power, cost, gold) {
-        this.type = type;
-        this.name = name;
-        this.power = power;
-        this.cost = cost;
-        this.gold = gold
-    }
-    buyShip() {
-        // if the user is human add the army into human army
-        if(this.type === "Human") {
-            humanArmy.push(this.name);
-        } else {
-            cpuArmy.push(this.name);
-        }
-    }
-}
-
-
-
-
-(function humanUser() {
-    document.querySelectorAll(".army-type").forEach(e => e.removeEventListener("click", buyArmy));
-    document.querySelectorAll(".army-type").forEach(e => e.addEventListener("click", buyArmy));
-
-    let gold = 10;
-
-    
-    function buyArmy() {
-        // the cost of the selected army taken from armySelection
-        const armyCost = armySelection[this.innerText];
-
-        if(gold >= armyCost) {
-            const spaceShip = new Unities ("Human", this.innerText, "??", armyCost, gold);
-            gold -= armyCost;
-            resources.innerHTML = "You Have " + gold + " Gold to craft your army";
-            spaceShip.buyShip();
-        } else {
-            return resources.innerHTML = "You can't spend more than 10 gold to craft your army"
-        }
-    }
-})();
-
-
-(function cpuUser() {
-
-    let goldCpu = 10;
-    startGame.removeEventListener("click", cpuGenerator);
-    startGame.addEventListener("click", cpuGenerator);
-    function cpuGenerator() {
-        
-        while(goldCpu > 0) {
-            // take all the keys from armySelection
-            const army = Object.keys(armySelection);
-            //take a random type of army and find the cost in armyCost;
-            const randomArmy = army[Math.floor(Math.random() * army.length)];
-            const armyCost = armySelection[randomArmy];
-
-            if(goldCpu >= armyCost) {
-                const spaceShip = new Unities ("CPU",randomArmy, "??", armyCost, goldCpu);
-                spaceShip.buyShip();
-                goldCpu -= armyCost;
-            } else {
-                // if the armyCost is bigger than goldCpu then call the function again to generate another random army
-                return cpuGenerator();
-            }
-        }
-    }
-})();
+startGame.addEventListener("click", function() {
+    game.players[0].playerArmy.push(humanArmy.create());
+    game.players[1].playerArmy.push(cpuArmy.create())
+    game.createGame();
+});
 
 

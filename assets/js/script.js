431d17cd2adb4586eb47e983064053d302001454
stefandhub <stefandifede@gmail.com>
2021-07-18 22:16:00 +0200

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ const authentication = document.getElementById("authentication");
     method: 'GET', // or 'PUT'
     headers: {
         'Content-Type': 'application/json',
+        'Cookie': getCookie('sessionid')
     },
     })
     .then(response => response.json())
@@ -57,7 +58,20 @@ const authentication = document.getElementById("authentication");
     confirmButton.addEventListener("click", changeBoxContent2);
 })();
 
-
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 
 
